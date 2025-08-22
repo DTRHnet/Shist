7f2e5d86d8b24cefcc80a9ec01eadb58f73fe703
DTRHnet <admin@dtrh.net>
2025-08-21 21:42:48 -0400

    api endpoint fixes

--- api/categories/[...slug].ts
+++ api/categories/[...slug].ts
@@ -3,7 +3,7 @@ import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import { eq } from 'drizzle-orm';
 import ws from "ws";
-import * as schema from "../../shared/schema";
+import * as schema from "../schema";
 
 neonConfig.webSocketConstructor = ws;
 
